a2d3a0ab6c82c96e6039505f1f52ab915a555098
alrtn4 <45690266+alrtn4@users.noreply.github.com>
2020-07-28 16:22:12 +0430

    Updates

---
+++ precache-manifest.b8daf9a79a17d335bb9c211a25c935cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9753b32ca3902aba54f7c7551c14a287",
+    "url": "/resume-react/index.html"
+  },
+  {
+    "revision": "cd3bcecfef887dc0472d",
+    "url": "/resume-react/static/css/main.61d5eeff.chunk.css"
+  },
+  {
+    "revision": "3f7b07edf46c31f4d68b",
+    "url": "/resume-react/static/js/2.83cad5d8.chunk.js"
+  },
+  {
+    "revision": "e6cfdf27d55c9f6a357639f74b8dca31",
+    "url": "/resume-react/static/js/2.83cad5d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd3bcecfef887dc0472d",
+    "url": "/resume-react/static/js/main.7a4b6d3e.chunk.js"
+  },
+  {
+    "revision": "2eab88594988d414d350",
+    "url": "/resume-react/static/js/runtime-main.27456c11.js"
+  }
+]);
